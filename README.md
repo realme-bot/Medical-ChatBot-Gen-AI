d281dfe9d192af4b817950c74899a69c21d15b8d
Farhan Azam <realmeu319@gmail.com>
2025-10-19 14:21:08 +0530

    folder structure added

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # Medical-ChatBot-Gen-AI
 
-This is my end to end medical gen ai chat bot 
+This is my end to end medical gen ai chat bot
+
+Step 1: Create a conda environment
+
+Step 2: Install the requirements
+
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages,setup
+
+setup(
+    name='Generative AI Project',
+    version='0.0.0',
+    author='Md Farhan Azam',
+    author_email='realmeu319@gmail.com',
+    packages=find_packages(),
+    install_require=[]
+
+)
